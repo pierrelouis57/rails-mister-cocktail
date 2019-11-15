b90ea21d1c39bd154b598e2ec3329d4c447142fd
pierrelouis geoffroy <pierrelouis.geoffroy@gmail.com>
2019-11-15 17:19:27 +0100

    fini

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,11 +4,15 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
+  mount_uploader :photo, PhotoUploader
 
   def url
     if name.downcase === "mojito"
 
       url = "https://static.750g.com/images/622-auto/b520523117d647dab6b842a36f4cc7f5/mojito-le-vrai.jpg"
+    elsif
+      name.downcase === "cuba libre"
+      url =  "cubalibre.jpg"
     else
       url = "https://images-na.ssl-images-amazon.com/images/I/51IJZB8Cv6L.jpg"
     end
